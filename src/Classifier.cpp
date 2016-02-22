f32a9fa2a77f5c0e05e7e4016d5aa377d7a81043
Jeremy Salwen <jeremysalwen@gmail.com>
2016-02-22 02:07:33 -0500

    clean up denormalization

--- src/Classifier.cpp
+++ src/Classifier.cpp
@@ -2,13 +2,13 @@
 
 namespace Halite {
   template<typename D>
-  void Classifier<D>::normalize() {
+  void Classifier<D>::denormalize() {
     if(normalization) {
       for(BetaCluster<D>& b:betaClusters) {
 	normalization->normalize(b.min.begin(), b.min.begin());
 	normalization->normalize(b.max.begin(), b.max.begin());
       }
-      normalization=NULL;
+      normalization.reset();
     }
   }
   template class Classifier<float>;
